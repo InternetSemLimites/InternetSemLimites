0ca2d6f8496d790552c019e11b82f31eefcadd54
Felipe Braz <fbraz3@gmail.com>
2016-04-14 17:12:10 -0300

    update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,12 +1,26 @@
-#!/bin/bash
+#!/bin/bash -x
 cd /home/automacao/InternetSemLimites && (
 	# Reset de segurança
 	git reset --hard HEAD; git clean -f -d; /usr/bin/git pull origin HEAD
 
 	#substitui o arquivo com o novo
-	#wget http://internetsemlimites.herokuapp.com/readme.md -O README.md 
+	wget https://internetsemlimites.herokuapp.com/README.md -O README.md.tmp
 
-	# commit e push
-	#git commit -a -m "readme atualizado"
-	#git push origin master
+        SIZE=`du -k "README.md.tmp" | cut -f 1`
+        if [ $SIZE -lt 1 ]; then
+                rm -f README.md.tmp
+		echo invalid file size
+		exit 0
+	fi
+
+        MD5A=`md5sum README.md |awk '{print $1}'`
+        MD5B=`md5sum README.md.tmp |awk '{print $1}'`
+
+	if [ "$MD5A" != "$MD5B" ]; then
+		mv -f README.md.tmp README.md
+		git pull origin master
+                git add .
+		git commit -a -m "auto-update README.md"
+                git push origin master
+	fi
 )
