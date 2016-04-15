eb1f3cb162af576e67b38c1f73fa1b04721055ec
Felipe Braz <fbraz3@gmail.com>
2016-04-15 12:13:23 -0300

    sync hall of shame =)

--- sync.sh
+++ sync.sh
@@ -1,24 +1,30 @@
 #!/bin/bash -x
+
+FILE="README.md"
+if [ "$1" == "shame" ]; then
+	FILE="HALL_OF_SHAME.md"
+fi
+
 cd /home/automacao/InternetSemLimites && (
 	# Reset de segurança
 	git reset --hard HEAD; git clean -f -d; /usr/bin/git pull origin HEAD
 
 	#substitui o arquivo com o novo
-	wget https://internetsemlimites.herokuapp.com/README.md -O README.md.tmp
+	wget https://internetsemlimites.herokuapp.com/$FILE -O $FILE.tmp
 
         SIZE=`du -k "README.md.tmp" | cut -f 1`
         if [ $SIZE -lt 1 ]; then
-                rm -f README.md.tmp
+                rm -f $FILE.tmp
 		echo invalid file size
 		exit 0
 	fi
 
-        MD5A=`md5sum README.md |awk '{print $1}'`
-        MD5B=`md5sum README.md.tmp |awk '{print $1}'`
+        MD5A=`md5sum $FILE |awk '{print $1}'`
+        MD5B=`md5sum $FILE.tmp |awk '{print $1}'`
 
 	if [ "$MD5A" != "$MD5B" ]; then
-		mv -f README.md.tmp README.md
+		mv -f $FILE.tmp $FILE
 		git pull origin master
-                git add . && (git commit -a -m "auto-update README.md"; git push origin master)
+                git add . && (git commit -a -m "auto-update $FILE"; git push origin master)
 	fi
 )
